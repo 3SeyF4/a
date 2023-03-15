10c902bfa8e968fa11bd71267e3ef930eab77fc1
Seyfullah Bağlıkol <sirusblack34@gmail.com>
2023-03-15 15:06:56 +0300

    header düzünlendi

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Header.css";
 
 const Header = ({ money, total }) => {
   return (
-    <div>
+    <div className="header">
       {(total > 0 && <>Harcamak için ${money - total} paranız kaldı.</>) || (
-        <>Harcamak için ${money} paranız var. </>
+        <>
+          Harcamak için <span>${money}</span> paranız var.{" "}
+        </>
       )}
     </div>
   );
